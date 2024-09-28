fa5bf147c138e34cafe3f802eecb18156a5c442b
kristofer111 <kristofers21@ru.is>
2024-09-28 15:30:39 +0000

    AudioPoolAdmin default value configured

--- AudioPool.Repository/Contexts/MusicDbContext.cs
+++ AudioPool.Repository/Contexts/MusicDbContext.cs
@@ -1,12 +1,36 @@
 using AudioPool.Models.Entities;
+using AudioPool.Models.InputModels;
 using Microsoft.EntityFrameworkCore;
 
 namespace AudioPool.Repository.Contexts
 {
     public class MusicDbContext : DbContext
     {
-        public MusicDbContext(DbContextOptions<MusicDbContext> options) : base(options) {}
-    
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Album>()
+                .Property(e => e.ModifiedBy)
+                .HasDefaultValue("AudioPoolAdmin");
+
+            modelBuilder.Entity<Artist>()
+            .Property(e => e.ModifiedBy)
+            .HasDefaultValue("AudioPoolAdmin");
+
+            modelBuilder.Entity<Genre>()
+            .Property(e => e.ModifiedBy)
+            .HasDefaultValue("AudioPoolAdmin");
+
+            modelBuilder.Entity<Song>()
+                .Property(e => e.ModifiedBy)
+                .HasDefaultValue("AudioPoolAdmin");
+
+
+        }
+
+        public MusicDbContext(DbContextOptions<MusicDbContext> options) : base(options) { }
+
         public DbSet<Album> Albums { get; set; }
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Genre> Genres { get; set; }
